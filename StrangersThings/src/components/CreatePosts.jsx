d4853a155ec14c6a843affeefcb724e39f801c9c
Malique Hammonds <107649903+Leekasaurus@users.noreply.github.com>
2023-08-28 19:04:40 -0400

    added logic

--- StrangersThings/src/components/CreatePosts.jsx
+++ StrangersThings/src/components/CreatePosts.jsx
@@ -1,4 +1,51 @@
 import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import { createPost} from "../api/posts";
-import {}
+import useAuth from "../hooks/useAuth";
+
+
+export default function NewPost() {
+    const navigate = useNavigate();
+    const {token} = useAuth();
+
+    const [title, setTitle] = useState([]);
+    const [description, setDescription] = useState([]);
+    const [price, setPrice] = useState([]);
+
+    return(
+        <div>
+            <h3> Make Post</h3>
+            <form>
+                <input 
+                type="text"
+                placeholder="title"
+                value="title"
+                onChange={(e) => {
+                    setTitle(e.target.value);
+                }}
+                ></input>
+            </form>
+            <form>
+                <input 
+                type="text"
+                placeholder="description"
+                value="description"
+                onChange={(e) => {
+                    setTitle(e.target.value);
+                }}
+                ></input>
+            </form>
+            <form>
+                <input 
+                type="text"
+                placeholder="price"
+                value="price"
+                onChange={(e) => {
+                    setTitle(e.target.value);
+                }}
+                ></input>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
